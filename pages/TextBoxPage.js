3abbc5ec1ac44c2cb282149b6d5821b832886e9d
markuschaaaa <chumakoff03@gmail.com>
2025-10-14 22:40:09 +0300

    fix: fixed text input test

--- pages/TextBoxPage.js
+++ pages/TextBoxPage.js
@@ -1,14 +1,27 @@
-class TextBox {
+import { expect } from '@playwright/test';
+
+export class TextBox {
   constructor(page) {
     this.page = page;
-    this.cards = page.locator('.card');
-    this.header = page.locator('div.banner img'); // пример
+    this.url = 'https://demoqa.com/text-box'
+    this.inputName = page.locator('input#userName');
+    this.inputEmail = page.locator('input#userEmail');
+    this.submitButton = page.locator('button#submit');
   }
   async goto() {
-    await this.page.goto('https://demoqa.com/');
+    await this.page.goto(this.url);
   }
-  async openCardByName(name) {
-    await this.page.locator(`.card:has-text("${name}")`).click();
+
+
+  async fillInputText(value){
+    await this.inputName.fill(value);
+    await expect(this.inputName).toHaveValue(value);
+  }
+
+  async fillInputEmail(value){
+    await this.inputEmail.fill(value);
+    await expect(this.inputEmail).toHaveValue(value);
+    await this.submitButton.click()
+    await expect(this.inputEmail).not.toHaveClass(/mr-sm-2 field-error form-control/);
   }
 }
-module.exports = { TextBox };
--- tests/textbox.spec.js
+++ tests/textbox.spec.js
@@ -1,19 +1,12 @@
-import { test, expect } from '@playwright/test';
-
-test('correct input', async ({ page }) => {
-  const nameInput = page.locator('input#userName');
-  await page.goto('https://demoqa.com/text-box');
-  await nameInput.fill('Hey There!');
-
-  // Expect a title "to contain" a substring.
-  await expect(nameInput).toHaveValue(/Hey There!/);
-});
+import { test } from '@playwright/test';
+import {TextBox} from '../pages/TextBoxPage.js'
 
 test('correct email', async ({ page }) => {
-  const nameInput = page.locator('input#userEmail');
-  const submitButton = page.locator('button#submit');
-  await page.goto('https://demoqa.com/text-box');
-  await nameInput.fill('test@gmail.com');
-  await submitButton.click()
-  await expect(nameInput).not.toHaveClass(/mr-sm-2 field-error form-control/);
+  const textBox = new TextBox(page)
+
+  await textBox.goto()
+
+  await textBox.fillInputText('Hey Hey Heyyyy!')
+
+  await textBox.fillInputEmail('chum@gmail.com')
 });
